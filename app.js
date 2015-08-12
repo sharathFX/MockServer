abe3e88794bb47dc1ee4bebc8514c1c83c193f98
Sharath Pandeshwar <sharath.pandeshwar@flipkart.com>
2015-08-12 08:46:45 +0530

    Commit 3 - Minor Bug Fix and Addition of configurable response code

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var os = require('os');
 var app = express();
+app.disable('etag')
 
 // ----------- Configuration Section ---------------
 
@@ -12,20 +14,32 @@ var INPUT_FILE = './output.json'
 /* Server listening port */
 var PORT = 8888
 
+/* What response code you want? */
+var RESPONSE_CODE = 200;
+
 // ----------- End of configuration Section ---------------
 
 var OUTPUT_JSON = require(INPUT_FILE);
 
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+//console.log(addresses);
+
 app.get('/' + ENDPOINT, function(req, res){
   console.log("Processing you request ...");
-  res.send(OUTPUT_JSON);
-  //console.log(OUTPUT_JSON);
+  res.send(RESPONSE_CODE, OUTPUT_JSON);
   console.log("");
 });
 
 app.listen(PORT);
+console.log("The API is waiting for you at http://"  + addresses[0] + ":" + PORT + "/"+ ENDPOINT);
 
-require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  var ipAddress = 'http://'+add + ':' + PORT
-  console.log("The API is waiting for you at " + ipAddress + "/" + ENDPOINT);
-})
